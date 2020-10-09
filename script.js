1e7580a9916f36b60f8ba88d1ba481a9bacb1c43
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-10-08 20:52:31 -0700

    initial commit

--- script.js
+++ script.js
@@ -4,18 +4,6 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-
-// button click activates series of window prompts
-
-// password criteria prompt
-
-// length criteria prompt (8-128)
-
-// character type prompt (lowercase, uppercase, numeric, and/or special characters)
-
-// input validated, one character type selected
-
 
 function writePassword() {
   var password = generatePassword();
