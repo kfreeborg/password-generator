36d686fde54cfdff10cc66e55a9cb383f8052451
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-10-09 18:20:33 -0700

    finished validations for password requirements

--- script.js
+++ script.js
@@ -29,15 +29,26 @@ function generatePassword() {
   }
   console.log("length", length, typeof length);
 
-  // confirm: lowercase option to type in lowercase, uppercase, or both (minute 28ish)
+  // confimr lowercase or uppercase or both
+  // var lettering = prompt("Would you like lowercase or uppercase lettering?\nPlease type in LOWERCASE or UPPERCASE.\nClick cancel for BOTH.", "LOWERCASE")
+  // console.log("lettering", lettering, typeof lettering);
+  // if (lettering !== "LOWERCASE" && lettering !== "UPPERCASE") {
+  //   alert("Invalid! Please specify LOWERCASE or UPPERCASE.");
+  //   return "";
+  // }
+
+  // confirm: lowercase
   var lowercase = confirm("Click OK if you want lowercase letters.");
   console.log("lowercase", lowercase, typeof lowercase);
 
-  // validate lowercase
-
-  // confirm: uppercase will have to skip if lowercase is false
+  // confirm: uppercase
   var uppercase = confirm("Click OK if you want uppercase letters.");
   console.log("uppercase", uppercase, typeof uppercase);
+
+  if (lowercase === false && uppercase === false) {
+    alert("Invalid! Please click OK for at least one lettering option!");
+    return "";
+  };
 
   // confirm: numeric
   var numeric = confirm("Click OK if you want numbers.");
@@ -48,9 +59,15 @@ function generatePassword() {
   console.log("special", special, typeof special);
 
   // generate a password
+  // length: 8-128 
+  // leading digit is a letter
+  // lowercase: if true include lowercase if false don't inclue
+  // uppercase: include uppercase if true, not if false
+  // numeric: include numbers if true, not if false
+  // special characters: include characters if true, not if false
 
 
-  return "password";
+  return "";
 }; // end function generatePassword
 
 // Add event listener to generate button
