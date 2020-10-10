d5028915a9a0b7dfe249fa107ee60a4c3c775a99
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-10-09 18:34:10 -0700

    added arrays

--- script.js
+++ script.js
@@ -66,6 +66,11 @@ function generatePassword() {
   // numeric: include numbers if true, not if false
   // special characters: include characters if true, not if false
 
+  // make arrays
+  var allLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var allUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var allDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];]
+  var allSymbols = ["!", "#", "$", "%", "&", "'", ")", "(", "*", "+", "-", "/", ":", ";", "<", ">", "=", "?", "@"];
 
   return "";
 }; // end function generatePassword
